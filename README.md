9b9278a1db5de59e392a5c0145c36a228ec4ec98
JoaoPauloFZ845 <rm99279@fiap.com.br>
2023-04-25 11:19:32 -0300

    aaaaa

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-###Exercitando o versionamento
+### Exercitando o versionamento
 
 - 1) Integrando repositórios [LOCAIS e REMOTOS]...
+- 2) Eventos com AddEventListener
+- 3) Estruturas de Repetição
+- 4) Estruturas de Decisão
+- 5) Operadores, Lógicos e Relacionais.
+- 6) Operador SPREAD.
+- 7) Array + HTML-Collections
+- 3) Funções Básicas + Arrow Functions.
 
--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,42 +1,96 @@
+ 
+ 
+// // for (let index = 0; index < liElement.length ; index++) {
+// //     console.log(liElement[index].textContent)
+// // }
+// // // console.log(liElement.length)
 
-// const liElement = document.getElementsByTagName("li");
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // // let nr3 = nr1+nr2;
+// // let nr3 = [nr1,nr2];
 
-// for (let index = 0; index<liElement.length; index++){
-//     console.log(liElement[index].textContent)
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
-// }
+// // //Listando um array! Técnica especial
+// // // nr3.forEach((itemDoaArray)=>{
+// // //     console.log(`Array-3 sendo listado ${itemDoaArray}`);
+// // // })
+// // // Listando o array com MAP
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+// //     numero.forEach((digitos)=>{
+// //         console.log(digitos)
+// //     })
+// // })
 
-//console.log(liElement.length)
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // //Espalhar o array com SPREAD [ ... ]
+// // let nr3 = [...nr1,...nr2];
 
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
-// declaração de arrays
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-let nr3 = [...nr1,...nr2];
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// // })
 
-//Listando um aray! Técnica especial
-// nr3.forEach((itemDoArray) =>{
-//     console.log(`Array-3 sendo listado ${itemDoArray}`);
+// //Convertendo HTMLCollection em Array de forma indireta
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml];
+
+// liElementArray.map((item,key)=>{
+//     if (item.textContent == "Item-39") {
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
 // })
 
-//Listando o array com MAP
-nr3.map((numero,key)=>{
-    console.log(`${(key+1)} - Itens do array - ${numero}`);
+// function soma(p1,p2,p3) {
+//     nr1 = p1;
+//     nr2 = p2;
+//     resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
+//     console.log(`O Resultado é : ${resultado}.`)
+//     return (resultado)
+    // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+    // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
+// }
+
+//ARROW FUNCTION
+// const mudaCor = ()=>{
     
-})
+    //FUNÇÃO SET-TIME-OUT
+//     setTimeout(soma, 5000, 100,100,100);
+// }
 
-//convertando HTMLCollection em Array de forma indireta
-const liElementHtml = document.getElementsByTagName("li");
-const liElementArray = [...liElementHtml];
+// mudaCor()
 
-console.log(liElementHtml)
+function mudaCor(){
 
-liElementArray.map((item,key)=>{
-    if(item.textContent == "Item-39"){
-    console.log(`${key+1} -${item.textContent}`)
-    }    
-})
+    let r = "";
+    let g = "";
+    let b = "";
+
+    // random= Gera números aleatórios entre 0 e 1.
+    //ceil, floor e round que arredondam o número para cima, para baixo ou aleatóriamente
+    //Determinando para a váriavel r um valor entre 0 e 255.
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    const cabecalho = document.querySelector(".cabecalho");
+
+    cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
+
+    tmp = setTimeout(mudaCor, 0.1)
+
+}
+
+mudaCor();
+
