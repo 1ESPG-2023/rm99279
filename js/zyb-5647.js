728d98d80d16e3baa6391fcf92d9affb447c21ce
JoaoPauloFZ845 <rm99279@fiap.com.br>
2023-05-02 11:49:33 -0300

    v2

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -52,21 +52,111 @@
 //     }
 // })
 
-function soma(p1,p2,p3) {
-    nr1 = p1;
-    nr2 = p2;
-    resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
-    console.log(`O Resultado é : ${resultado}.`)
-    return (resultado)
-    // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
-    // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
-}
+// function soma(p1,p2,p3) {
+//     nr1 = p1;
+//     nr2 = p2;
+//     resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
+//     console.log(`O Resultado é : ${resultado}.`)
+//     return (resultado)
+//     // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+//     // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
+// }
 
-//ARROW FUNCTION
-const mudaCor = ()=>{
+// //ARROW FUNCTION
+// const mudaCor = ()=>{
     
-    //FUNÇÃO SET-TIME-OUT
-    setTimeout(soma, 5000, 100,100,100);
+//     //FUNÇÃO SET-TIME-OUT
+//     setTimeout(soma, 5000, 100,100,100);
+// }
+
+// mudaCor()
+
+let tmp = "";
+
+function mudaCor() {
+    
+    let r = "";
+    let g = "";
+    let b = "";
+
+    //random = Gera números aleatórios entre 0 e 1
+    //ceil,floor e round que arredondam o número para cima, para baixo ou aleatóriamente.
+
+    //Determinando para a variável r um valor entre 0 e 255.
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    const cabecalho = document.querySelector(".cabecalho");
+    
+    cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`)
+
+    tmp = setTimeout(mudaCor, 1000);
+
 }
 
-mudaCor()
+
+//FUNÇÃO ALTERA BANNER
+// function alteraBanner() {
+    
+    
+//     const img1 = document.querySelector(".l-e > img")
+//     const img2 = document.querySelector(".l-d > img")
+    
+//     let nr = Math.ceil(Math.random() *3);
+//     let caminho = `./img/banner-lateral-${nr}.png`;
+//     img1.src = caminho;
+    
+//     nr = Math.ceil(Math.random() *3);
+//     caminho = `./img/banner-lateral-${nr}.png`;
+//     img2.src = caminho;
+
+//     setTimeout(alteraBanner, 1000);
+
+// }
+
+// alteraBanner();
+
+// const botao = document.querySelector(".central button");
+
+// botao.addEventListener("click" , ()=>{
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(this.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     }else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// });
+
+// function ligaDesliga() {
+
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(botao.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     }else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// }
+
+const inputUser = document.querySelector("input[name='nmUser']");
+const labelUser = document.querySelector("label[for]");
+console.log(labelUser);
+
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style", "outline-color:#ff0000");
+});
+
+inputUser.addEventListener("keyup", ()=>{
+
+    if(inputUser.value.length < 8){
+        inputUser.setAttribute("style", "outline-color:#ff0000");
+    }else{
+        inputUser.setAttribute("style", "outline-color:#dddddd");
+    }
+});
